095a74b45432f6b0d8794353def5b0e4b96edd96
Anthony Shek <antsh3k@users.noreply.github.com>
2020-10-01 23:37:03 +0100

    cogstack2csv initial commit

--- cogstack.py
+++ cogstack.py
@@ -2,8 +2,8 @@ import getpass
 import elasticsearch
 import elasticsearch.helpers
 import pandas as pd
-
 from typing import Dict, List
+
 
 class CogStack(object):
     def __init__(self, host, port=9200, username=None, password=None, scheme='https'):
@@ -55,3 +55,42 @@ def cogstack2df(cogstack_search_gen, column_headers=None):
         results.append(result)
     df_results = pd.DataFrame(results[1:], columns=results[0])
     return df_results
+
+
+def cogstack2csv(cogstack_search_gen, filename, column_headers=None):
+    # TODO: Complete function for chunking large searches.
+    """
+    Returns CSV from CogStack search
+
+    :param cogstack_search_gen: CogStack output generator object
+    :param filename: .CSV filename
+    :param column_headers: specify column headers
+    :return: CSV
+    """
+    counter = 100000
+    results = [column_headers]
+    for i, doc in enumerate(cogstack_search_gen):
+        if column_headers is None:
+            column_headers = ['id'] + list(doc['_source'].keys())
+            results = [column_headers]
+        result = []
+        for col in column_headers:
+            if col == 'id':
+                result.append(doc['_id'])
+            else:
+                result.append(doc['_source'].get(col, ''))
+        results.append(result)
+
+        if len(results) > counter:
+            counter += 100000
+            print(f"{counter} to csv")
+            df_results = pd.DataFrame(results[1:], columns=results[0])
+            df_results.to_csv(filename, header=column_headers, index=False, mode='a', chunksize=10000)
+            results = []
+
+    if df_results is None:
+        pd.DataFrame({}).to_csv(filename, header=column_headers, index=False)  # TODO check if necessary
+    df_results = pd.DataFrame(results[1:], columns=results[0])
+    df_results.to_csv(filename, header=column_headers, index=False, mode='a', chunksize=10000)
+    return
+
